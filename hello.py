c254342c32d9ad67a102418f36cea1ac9067702d
Elton Ago <agoe1@montclair.edu>
2023-07-11 18:30:36 -0400

    test first commit

---
+++ hello.py
@@ -0,0 +1,7 @@
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.datasets import make_regression
+X, y = make_regression(n_features=4, n_informative=2, random_state=0, shuffle=False)
+regr = RandomForestRegressor(max_depth=2, random_state=0)
+regr.fit(X, y)
+RandomForestRegressor()
+print(regr.predict([[0, 0, 0, 0]]))
